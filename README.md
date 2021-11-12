ab0f05978721939726176c3dc867a069f5e6c777
wchr-aun <wchr.aun@gmail.com>
2021-11-12 02:03:29 +0000

    :sparkles: (tiny_gp.py) to modify tiny_gp code

--- README.md
+++ README.md
@@ -1,36 +1,5 @@
-# Tiny Genetic Programming in Python
+# Tiny Genetic Programming in Python (Modified)
 
-A minimalistic program implementing Koza-style (tree-based) genetic programming to solve a symbolic regression problem. 
+A modified version of [Tiny Genetic Programming in Python](https://github.com/moshesipper/tiny_gp)
 
-**tiny-gp.py** is a basic (and fully functional) version, which produces textual output of the evolutionary progression and evolved trees.
-
-**tiny-gp-plus.py** displays dynamic graphs of error and mean tree size (size = number of nodes), has a bloat-control option, and produces nicer, graphic output (you'll need to install https://pypi.org/project/graphviz/).
-
-If you wish to cite this:
-```
-@misc{Sipper2019tinyGP,
-  author = {Sipper, M.},
-  title = {Tiny Genetic Programming in Python},
-  year = {2019},
-  publisher = {GitHub},
-  journal = {GitHub repository},
-  howpublished = {\url{https://github.com/moshesipper/tiny_gp} }
-}
-```
-
-| Symbolic | Regression using GP  |
-|-------------:|:-------------| 
-| Objective | Find an expression with one input (independent variable x), whose output equals the value of the quartic function  x<sup>4</sup> + x<sup>3</sup> + x<sup>2</sup> + x + 1 |
-| Function set | add, sub, mul |   
-| Terminal set | x, -2, -1, 0, 1, 2  |   
-| Fitness | Inverse mean absolute error over a dataset of 101 target values, normalized to [0,1]
-| Paremeters | POP_SIZE (population size), MIN_DEPTH (minimal initial random tree depth), MAX_DEPTH (maximal initial random tree depth), GENERATIONS (maximal number of generations), TOURNAMENT_SIZE (size of tournament for tournament selection), XO_RATE (crossover rate), PROB_MUTATION (per-node mutation probability) |
-| Termination | Maximal number of generations reached or an individual with fitness = 1.0 found |
-
-| Evolved solution | Another evolved solution |
-|-------------|-------------| 
-| ![GPTree](https://github.com/moshesipper/tiny-gp/blob/master/Figures/GPTree.png) | ![GPTree2](https://github.com/moshesipper/tiny-gp/blob/master/Figures/GPTree2.png) |
-
-| Bloat control  | No bloat control |
-|-------------|-------------| 
-|![GP run](https://github.com/moshesipper/tiny-gp/blob/master/Figures/with_bloat_control.png) | ![GP run](https://github.com/moshesipper/tiny-gp/blob/master/Figures/without_bloat_control.png) |
+It was modified to be able to run a coursework of Natural Computing (INFR11161).
--- tiny_gp.py
+++ tiny_gp.py
@@ -1,137 +1,214 @@
-# tiny genetic programming by © moshe sipper, www.moshesipper.com
-from random import random, randint, seed
+### THIS CODE IS MODIFIED FROM https://github.com/moshesipper/tiny_gp BY moshesipper
+
+import math
+import numpy as np
+from random import random, randint
 from statistics import mean
 from copy import deepcopy
+import time
 
-POP_SIZE        = 60   # population size
-MIN_DEPTH       = 2    # minimal initial random tree depth
-MAX_DEPTH       = 5    # maximal initial random tree depth
-GENERATIONS     = 250  # maximal number of generations to run evolution
-TOURNAMENT_SIZE = 5    # size of tournament for tournament selection
-XO_RATE         = 0.8  # crossover rate 
-PROB_MUTATION   = 0.2  # per-node mutation probability 
+POP_SIZE        = 120
+MIN_DEPTH       = 2
+MAX_DEPTH       = 5
+GENERATIONS     = 300
+TOURNAMENT_SIZE = 10
+XO_RATE         = 0.7
+PROB_MUTATION   = 0.3
+FILE_NAME       = './cw1-q4-ds.txt' # File name
+ITERATIONS       = 10 # Amount of iterations to run. For 1 time run, set this value to 1
 
 def add(x, y): return x + y
-def sub(x, y): return x - y
 def mul(x, y): return x * y
-FUNCTIONS = [add, sub, mul]
-TERMINALS = ['x', -2, -1, 0, 1, 2] 
+def sub(x, y): return x - y
+def div(x,y):
+  if math.isclose(y, 0):
+    return 1
+  return x/y
+def sin(x, y): return math.sin(x*y)
+def cos(x, y): return math.cos(x*y)
+def exp(x):
+  try:
+    return math.exp(x)
+  except:
+    return 0
+def abso(x): return abs(x)
+def power(x, n):
+  if n != 1 and n != 2 and n != 3 and n != 4:
+    return 0
+  try:
+    return math.pow(x, n)
+  except:
+    return 0
+def powerth(x, n):
+  if n != 1 and n != 2 and n != 3 and n != 4:
+    return 0
+  try:
+    return math.pow(x, 1/n)
+  except:
+    return 0
 
-def target_func(x): # evolution's target
-    return x*x*x*x + x*x*x + x*x + x + 1
+OPERATIONS = [add, sub, mul, div, sin, cos, exp, abso, power, powerth]
+FUNCTIONS = [exp, abso]
+TERMINALS = ['x', 'y', 1, 2, 3, 4, 10, 100, math.pi]
 
-def generate_dataset(): # generate 101 data points from target_func
-    dataset = []
-    for x in range(-100,101,2): 
-        x /= 100
-        dataset.append([x, target_func(x)])
-    return dataset
+def clearLog():
+  f = open('./lg.txt', 'w')
+  f.write('')
+  f.close()
+
+def log(s):
+  f = open('./lg.txt', 'a')
+  f.write(s +'\n')
+  f.close()
+
+def generate_dataset():
+  f = open(FILE_NAME, 'r')
+  ds = f.read()
+  f.close()
+  return [list(map(float, data.split())) for data in ds.split('\n')][:-1]
+
+def is_number(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
+
+def symbols(opt, x='', y=''):
+  x = 'pi' if is_number(x) and math.isclose(float(x), math.pi) else x
+  y = 'pi' if is_number(y) and math.isclose(float(y), math.pi) else y
+  if opt == 'add': return f'{x}+{y}'
+  elif opt == 'mul': return f'{x}*{y}'
+  elif opt == 'sub': return f'{x}-{y}'
+  elif opt == 'div': return f'{x}/{y}'
+  elif opt == 'power': return f'{x}^{y}'
+  elif opt == 'powerth': return f'{x}^(1/{y})'
+  elif opt == 'cos': return f'cos(({x})*({y}))'
+  elif opt == 'sin': return f'sin(({x})*({y}))'
+  elif opt == 'abso': return f'abs({x})'
+  elif opt == 'exp': return f'exp({x})'
+  else: return opt
 
 class GPTree:
-    def __init__(self, data = None, left = None, right = None):
-        self.data  = data
-        self.left  = left
-        self.right = right
-        
-    def node_label(self): # string label
-        if (self.data in FUNCTIONS):
-            return self.data.__name__
-        else: 
-            return str(self.data)
-    
-    def print_tree(self, prefix = ""): # textual printout
-        print("%s%s" % (prefix, self.node_label()))        
-        if self.left:  self.left.print_tree (prefix + "   ")
-        if self.right: self.right.print_tree(prefix + "   ")
+  def __init__(self, data = None, left = None, right = None):
+    self.data  = data
+    self.left  = left
+    self.right = right
 
-    def compute_tree(self, x): 
-        if (self.data in FUNCTIONS): 
-            return self.data(self.left.compute_tree(x), self.right.compute_tree(x))
-        elif self.data == 'x': return x
-        else: return self.data
-            
-    def random_tree(self, grow, max_depth, depth = 0): # create random tree using either grow or full method
-        if depth < MIN_DEPTH or (depth < max_depth and not grow): 
-            self.data = FUNCTIONS[randint(0, len(FUNCTIONS)-1)]
-        elif depth >= max_depth:   
-            self.data = TERMINALS[randint(0, len(TERMINALS)-1)]
-        else: # intermediate depth, grow
-            if random () > 0.5: 
-                self.data = TERMINALS[randint(0, len(TERMINALS)-1)]
-            else:
-                self.data = FUNCTIONS[randint(0, len(FUNCTIONS)-1)]
-        if self.data in FUNCTIONS:
-            self.left = GPTree()          
-            self.left.random_tree(grow, max_depth, depth = depth + 1)            
-            self.right = GPTree()
-            self.right.random_tree(grow, max_depth, depth = depth + 1)
+  def node_label(self): # string label
+    if (self.data in OPERATIONS):
+      return self.data.__name__
+    else: 
+      return str(self.data)
 
-    def mutation(self):
-        if random() < PROB_MUTATION: # mutate at this node
-            self.random_tree(grow = True, max_depth = 2)
-        elif self.left: self.left.mutation()
-        elif self.right: self.right.mutation() 
+  def formula(self):
+    if self.left and self.right: return symbols(self.node_label(), self.left.formula(), self.right.formula())
+    elif self.left: return symbols(self.node_label(), self.left.formula())
+    else: return symbols(self.node_label())
 
-    def size(self): # tree size in nodes
-        if self.data in TERMINALS: return 1
-        l = self.left.size()  if self.left  else 0
-        r = self.right.size() if self.right else 0
-        return 1 + l + r
+  def compute_tree(self, x, y): 
+    if (self.data in OPERATIONS):
+      if self.data in FUNCTIONS:
+        return self.data(self.left.compute_tree(x, y))
+      else:
+        return self.data(self.left.compute_tree(x, y), self.right.compute_tree(x, y))
+    elif self.data == 'x': return x
+    elif self.data == 'y': return y
+    else: return self.data
+          
+  def random_tree(self, grow, max_depth, depth = 0): # create random tree using either grow or full method
+    if depth < MIN_DEPTH or (depth < max_depth and not grow): 
+      self.data = OPERATIONS[randint(0, len(OPERATIONS)-1)]
+    elif depth >= max_depth:   
+      self.data = TERMINALS[randint(0, len(TERMINALS)-1)]
+    else: # intermediate depth, grow
+      if random () > 0.5: 
+        self.data = TERMINALS[randint(0, len(TERMINALS)-1)]
+      else:
+        self.data = OPERATIONS[randint(0, len(OPERATIONS)-1)]
+    if self.data in OPERATIONS:
+      if self.data in FUNCTIONS:
+        self.left = GPTree()          
+        self.left.random_tree(grow, max_depth, depth = depth + 1)
+      else:
+        self.left = GPTree()          
+        self.left.random_tree(grow, max_depth, depth = depth + 1)            
+        self.right = GPTree()
+        self.right.random_tree(grow, max_depth, depth = depth + 1)
 
-    def build_subtree(self): # count is list in order to pass "by reference"
-        t = GPTree()
-        t.data = self.data
-        if self.left:  t.left  = self.left.build_subtree()
-        if self.right: t.right = self.right.build_subtree()
-        return t
-                        
-    def scan_tree(self, count, second): # note: count is list, so it's passed "by reference"
-        count[0] -= 1            
-        if count[0] <= 1: 
-            if not second: # return subtree rooted here
-                return self.build_subtree()
-            else: # glue subtree here
-                self.data  = second.data
-                self.left  = second.left
-                self.right = second.right
-        else:  
-            ret = None              
-            if self.left  and count[0] > 1: ret = self.left.scan_tree(count, second)  
-            if self.right and count[0] > 1: ret = self.right.scan_tree(count, second)  
-            return ret
+  def mutation(self):
+    if random() < PROB_MUTATION: # mutate at this node
+        self.random_tree(grow = True, max_depth = 2)
+    elif self.left: self.left.mutation()
+    elif self.right: self.right.mutation() 
 
-    def crossover(self, other): # xo 2 trees at random nodes
-        if random() < XO_RATE:
-            second = other.scan_tree([randint(1, other.size())], None) # 2nd random subtree
-            self.scan_tree([randint(1, self.size())], second) # 2nd subtree "glued" inside 1st tree
-# end class GPTree
-                   
-def init_population(): # ramped half-and-half
-    pop = []
-    for md in range(3, MAX_DEPTH + 1):
-        for i in range(int(POP_SIZE/6)):
-            t = GPTree()
-            t.random_tree(grow = True, max_depth = md) # grow
-            pop.append(t) 
-        for i in range(int(POP_SIZE/6)):
-            t = GPTree()
-            t.random_tree(grow = False, max_depth = md) # full
-            pop.append(t) 
-    return pop
+  def size(self): # tree size in nodes
+    if self.data in TERMINALS: return 1
+    l = self.left.size()  if self.left  else 0
+    r = self.right.size() if self.right else 0
+    return 1 + l + r
+
+  def build_subtree(self): # count is list in order to pass 'by reference'
+    t = GPTree()
+    t.data = self.data
+    if self.left:  t.left  = self.left.build_subtree()
+    if self.right: t.right = self.right.build_subtree()
+    return t
+                      
+  def scan_tree(self, count, second): # note: count is list, so it's passed 'by reference'
+    count[0] -= 1            
+    if count[0] <= 1: 
+      if not second: # return subtree rooted here
+        return self.build_subtree()
+      else: # glue subtree here
+        self.data  = second.data
+        self.left  = second.left
+        self.right = second.right
+    else:  
+      ret = None              
+      if self.left  and count[0] > 1: ret = self.left.scan_tree(count, second)  
+      if self.right and count[0] > 1: ret = self.right.scan_tree(count, second)  
+      return ret
+
+  def crossover(self, other): # xo 2 trees at random nodes
+    if random() < XO_RATE:
+      second = other.scan_tree([randint(1, other.size())], None) # 2nd random subtree
+      self.scan_tree([randint(1, self.size())], second) # 2nd subtree 'glued' inside 1st tree
+
+  def depth(self):     
+    if self.data in TERMINALS: return 0
+    l = self.left.depth()  if self.left  else 0
+    r = self.right.depth() if self.right else 0
+    return 1 + max(l, r)
 
 def fitness(individual, dataset): # inverse mean absolute error over dataset normalized to [0,1]
-    return 1 / (1 + mean([abs(individual.compute_tree(ds[0]) - ds[1]) for ds in dataset]))
-                
+  return 1 / (1 + mean([abs(individual.compute_tree(ds[0], ds[1]) - ds[2]) + 0.01*individual.size() for ds in dataset]))
+
 def selection(population, fitnesses): # select one individual using tournament selection
-    tournament = [randint(0, len(population)-1) for i in range(TOURNAMENT_SIZE)] # select tournament contenders
-    tournament_fitnesses = [fitnesses[tournament[i]] for i in range(TOURNAMENT_SIZE)]
-    return deepcopy(population[tournament[tournament_fitnesses.index(max(tournament_fitnesses))]]) 
-            
-def main():      
-    # init stuff
-    seed() # init internal state of random number generator
+  tournament = [randint(0, len(population)-1) for i in range(TOURNAMENT_SIZE)] # select tournament contenders
+  tournament_fitnesses = [fitnesses[tournament[i]] for i in range(TOURNAMENT_SIZE)]
+  return deepcopy(population[tournament[tournament_fitnesses.index(max(tournament_fitnesses))]]) 
+
+def init_population(): # ramped half-and-half
+  pop = []
+  for md in range(3, MAX_DEPTH + 1):
+    for i in range(int(POP_SIZE/6)):
+      t = GPTree()
+      t.random_tree(grow = True, max_depth = md) # grow
+      pop.append(t) 
+    for i in range(int(POP_SIZE/6)):
+      t = GPTree()
+      t.random_tree(grow = False, max_depth = md) # full
+      pop.append(t) 
+  return pop
+
+def main():
+  restart = True
+  restart_count = 0
+  while restart:
+    restart = False
     dataset = generate_dataset()
-    population= init_population() 
+    population = init_population() 
     best_of_run = None
     best_of_run_f = 0
     best_of_run_gen = 0
@@ -139,27 +216,54 @@ def main():
 
     # go evolution!
     for gen in range(GENERATIONS):        
-        nextgen_population=[]
-        for i in range(POP_SIZE):
-            parent1 = selection(population, fitnesses)
-            parent2 = selection(population, fitnesses)
-            parent1.crossover(parent2)
-            parent1.mutation()
-            nextgen_population.append(parent1)
-        population=nextgen_population
-        fitnesses = [fitness(population[i], dataset) for i in range(POP_SIZE)]
-        if max(fitnesses) > best_of_run_f:
-            best_of_run_f = max(fitnesses)
-            best_of_run_gen = gen
-            best_of_run = deepcopy(population[fitnesses.index(max(fitnesses))])
-            print("________________________")
-            print("gen:", gen, ", best_of_run_f:", round(max(fitnesses),3), ", best_of_run:") 
-            best_of_run.print_tree()
-        if best_of_run_f == 1: break   
-    
-    print("\n\n_________________________________________________\nEND OF RUN\nbest_of_run attained at gen " + str(best_of_run_gen) +\
-          " and has f=" + str(round(best_of_run_f,3)))
-    best_of_run.print_tree()
-    
-if __name__== "__main__":
-  main()
+      nextgen_population=[]
+      for i in range(POP_SIZE):
+        parent1 = selection(population, fitnesses)
+        parent2 = selection(population, fitnesses)
+        parent1.crossover(parent2)
+        parent1.mutation()
+        nextgen_population.append(parent1)
+      population=nextgen_population
+      fitnesses = [fitness(population[i], dataset) for i in range(POP_SIZE)]
+      if max(fitnesses) > best_of_run_f:
+        best_of_run_f = max(fitnesses)
+        best_of_run_gen = gen
+        best_of_run = deepcopy(population[fitnesses.index(best_of_run_f)])
+        raw_fitness = 1 / (1 + mean([abs(best_of_run.compute_tree(ds[0], ds[1]) - ds[2]) for ds in dataset]))
+        print('________________________')
+        print(f'gen: {gen} best_of_run_f: {round(best_of_run_f,3)} raw_fitness: {round(raw_fitness, 3)}')
+        print(f'Formula: {best_of_run.formula()}')
+      if raw_fitness > 0.9 and gen - best_of_run_gen > 50: break # Let GP tries to simplify the formula for another 50 generations
+      if (gen > 50 and best_of_run_f < 0.6) or (gen > 200 and best_of_run_f < 0.7): # If the fitness value does not reach some value at certain point, restart the run
+      # if (gen > 50 and best_of_run_f < 0.6): # This condition will definitely take less (or equal) time to the above condition, but may not find the closest formula
+        restart_count += 1
+        restart = True
+        break
+
+  raw_fitness = 1 / (1 + mean([abs(best_of_run.compute_tree(ds[0], ds[1]) - ds[2]) for ds in dataset]))
+  # print('-------------------------------------------------')
+  print(f'-END OF RUN-\n')
+  print(f'best_of_run attained at generation {best_of_run_gen}th of {restart_count}th restart.')
+  print(f'Have the fitness value of {round(best_of_run_f, 3)} and raw fitness value of {round(raw_fitness, 3)}')
+  print(f'Final Formula: {best_of_run.formula()}')
+  return raw_fitness
+
+if __name__== '__main__':
+  clearLog()
+  times = []
+  _fitnesses = []
+  for i in range(ITERATIONS):
+    print('-------------------------------------------------')
+    print(f'-START OF {i + 1}th RUN-\n')
+    start = time.time()
+    _fitnesses.append(main())
+    end = time.time()
+    print(f'Time taken: {int((end - start) / 60)}:{str(int(end - start) % 60).zfill(2)} min\n')
+    times.append(end - start)
+    print(f'-END OF {i + 1}th RUN-\n')
+    print('-------------------------------------------------')
+  avg_time = np.array(times).mean()
+  avg_fitness = np.array(_fitnesses).mean()
+  print(f'\n\nAverage runtime over 10 runs is {int((avg_time) / 60)}:{str(int(avg_time) % 60).zfill(2)} min')
+  print(f'Average fitness over 10 runs is {avg_fitness}')
+  print(f'Average error over 10 runs is {1-avg_fitness}')
